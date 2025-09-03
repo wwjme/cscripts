4a262b9aceb2d3b2adf28fc50910abcd86535f7b
wwjme <menome0@proton.me>
2025-09-03 16:35:26 -0700

    name skin update

--- Citizen&Police/HalfBHalfG.js
+++ Citizen&Police/HalfBHalfG.js
@@ -9,26 +9,40 @@ function init(e) {
     var isMale = Math.random() < 0.5;
 
     if (isMale) {
-        var maleSkins = [
-            "cyberpunkskins:textures/b/b01",
-            "cyberpunkskins:textures/b/b02",
-            "cyberpunkskins:textures/b/b03",
-        ];
+        // Generate skins b01 → b34
+        var maleSkins = [];
+        for (var i = 1; i <= 34; i++) {
+            var num = (i < 10 ? "0" + i : i); // b01, b02 ... b34
+            maleSkins.push("cyberpunkskins:textures/b/b" + num + ".png");
+        }
+
         var maleNames = [
-            "John", "Michael", "David", "Chris", "Daniel", "Alex", "Robert", "James"
+            "John", "Michael", "David", "Chris", "Daniel", "Alex", "Robert", "James",
+            "William", "Joseph", "Anthony", "Mark", "Matthew", "Andrew", "Joshua", "Brian",
+            "Kevin", "Jason", "Justin", "Ryan", "Brandon", "Jacob", "Nicholas", "Eric",
+            "Jonathan", "Stephen", "Larry", "Scott", "Frank", "Tyler", "Dennis", "Jerry",
+            "Aaron", "Adam", "Patrick", "Sean", "Zachary", "Nathan", "Samuel", "Kyle",
+            "Benjamin", "Paul", "Ethan", "Gregory", "Jordan", "Cameron", "Dylan", "Hunter"
         ];
 
         display.setSkinTexture(randomFrom(maleSkins));
         display.setName(randomFrom(maleNames));
 
     } else {
-        var femaleSkins = [
-            "cyberpunkskins:textures/g/g01",
-            "cyberpunkskins:textures/g/g02",
-            "cyberpunkskins:textures/g/g03",
-        ];
+        // Generate skins g01 → g34
+        var femaleSkins = [];
+        for (var i = 1; i <= 34; i++) {
+            var num = (i < 10 ? "0" + i : i); // g01, g02 ... g34
+            femaleSkins.push("cyberpunkskins:textures/g/g" + num + ".png");
+        }
+
         var femaleNames = [
-            "Sarah", "Emily", "Anna", "Sophia", "Kate", "Maria", "Laura", "Emma"
+            "Sarah", "Emily", "Anna", "Sophia", "Kate", "Maria", "Laura", "Emma",
+            "Olivia", "Isabella", "Mia", "Amelia", "Charlotte", "Harper", "Evelyn", "Abigail",
+            "Ella", "Grace", "Chloe", "Victoria", "Natalie", "Hannah", "Lily", "Zoe",
+            "Samantha", "Leah", "Stella", "Claire", "Audrey", "Savannah", "Brooklyn", "Bella",
+            "Lucy", "Avery", "Scarlett", "Aria", "Ellie", "Maya", "Sofia", "Layla",
+            "Nora", "Riley", "Hazel", "Aurora", "Violet", "Penelope", "Lillian", "Naomi"
         ];
 
         display.setSkinTexture(randomFrom(femaleSkins));
