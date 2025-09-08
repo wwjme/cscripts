c18f0ce8c575b479ccc67b2fd701f5a94ffb7d20
wwjme <menome0@proton.me>
2025-09-08 01:17:16 -0700

    Update replace.js

--- Citizen&Police/replace.js
+++ Citizen&Police/replace.js
@@ -10,7 +10,7 @@ function init(e){
    reseted=1;
   }
   if(npc.getFaction().getId()==15 && reseted==1){
-   pos=npc.getPos();
+   var pos=npc.getPos();
    npc.world.spawnClone(pos.getX(), pos.getY(), pos.getZ(), 3, "CitizenPolice");
    npc.despawn();
    npc.say("daspawn");
