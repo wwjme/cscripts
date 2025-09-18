cb5fbd17cb6f648eceda6e1810a54684d9b01b4a
wwjme <menome0@proton.me>
2025-09-18 04:47:05 -0700

    Update target.js

--- enemies/bounty/lev1/target.js
+++ enemies/bounty/lev1/target.js
@@ -1,12 +1,32 @@
 // --- Configurable reward ---
 // Change these values for different items/rewards
+var skinAvail = 34;
 var rewardItem = "minecraft:emerald";  // item ID
 var rewardCount = 5;                   // how many
 
 function init(e){
     var npc = e.npc;
     var item = npc.world.createItem(rewardItem, rewardCount);
+    var display = npc.getDisplay();
+    
     npc.getInventory().setDropItem(1, item, 100);
+
+    var isMale = Math.random() < 0.5;
+
+    if (isMale) {
+        var maleSkins = [];
+        for (var i = 1; i <= skinAvail; i++) {
+            var num = (i < 10 ? "0" + i : i);
+            maleSkins.push("cyberpunkskins:textures/b/b" + num + ".png");
+        } 
+         display.setSkinTexture(randomFrom(maleSkins));    
+    } else {
+        var femaleSkins = [];
+        for (var i = 1; i <= skinAvail; i++) {
+            var num = (i < 10 ? "0" + i : i);
+            femaleSkins.push("cyberpunkskins:textures/g/g" + num + ".png");   
+        } 
+       display.setSkinTexture(randomFrom(femaleSkins));   
 }
 
 
